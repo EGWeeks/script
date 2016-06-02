68dc9bdd7e609b501114ed15b58d691214a4d5ed
Ethan Weeks <ethanweeks@ymail.com>
2016-06-02 00:30:17 -0600

    updated js file to use swtich statement

--- script.js
+++ script.js
@@ -8,25 +8,29 @@ function electric(kwhUsed){
 	// fixed charge per month
 	var fixedChargePerMonth = 5.37;
 	// current month getMonth is from 0 - 11
-	var currentDate = new Date()
+	var currentDate = new Date();
 	var month = currentDate.getMonth();
 
-	var firstFiveCost;
-	var secondFiveCost;
-	var additionalKWH;
-	var overOne;
-	var billCost;
-	var overFive;
-	var roundedCost;
+	var firstFiveCost,
+			secondFiveCost,
+			additionalKWH,
+			overOne,
+			billCost,
+			overFive,
+			roundedCost;
 
-	if ((month === 5) || (month === 6) || (month === 7)) {
-		firstFiveCost = 0.0894;
-		secondFiveCost = 0.10674;
-		additionalKWH = 0.14151;
-	} else {
-		firstFiveCost = 0.08300;
-		secondFiveCost = 0.08724;
-		additionalKWH = 0.09657;
+	switch (month) {
+		case 5:
+		case 6:
+		case 7:
+			firstFiveCost = 0.0894;
+			secondFiveCost = 0.10674;
+			additionalKWH = 0.14151;
+			break;
+		default:
+			firstFiveCost = 0.08300;
+			secondFiveCost = 0.08724;
+			additionalKWH = 0.09657;
 	}
 	
 	if (kwhUsed > 1000) {
